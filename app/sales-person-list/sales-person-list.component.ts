322866ed47288e5099106fef59b975c5c1f5d44d
naveenkumarn90 <navku1102@gmail.com>
2022-08-02 08:39:00 +0530

    add

---
+++ app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  constructor() { }
+
+    salesPersonList: Salesperson[]=[
+      new SalesPersonListComponent(),
+      new SalesPersonListComponent(),
+      new SalesPersonListComponent(),
+      new SalesPersonListComponent()
+
+    ]
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ app/sales-person/sales-person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person',
+  templateUrl: './sales-person.component.html',
+  styleUrls: ['./sales-person.component.css']
+})
+export class SalesPersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
